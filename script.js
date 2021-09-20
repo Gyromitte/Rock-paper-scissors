98c694179b70f5aa3743219ab6828abe7080d27e
Gyromitte <gyromitte@gmail.com>
2021-09-20 17:38:41 -0500

    Added game function and finished the project for the moment

--- script.js
+++ script.js
@@ -4,38 +4,52 @@ function computerPlay(){
     return computerChoice; 
 }
 
-let playerChoice = prompt("Write your choice: ");
-let fixPlayerChoice = playerChoice.toLowerCase();
+var playerScore=0;
+var cpuScore=0;
+
 
 function playRound(fixPlayerChoice, computerChoice){
+   
     //Paper 
    if(fixPlayerChoice == "paper" && computerChoice == "paper"){
+    playerScore++;
+    cpuScore++;
     return "it's a draw";
    }
    if(fixPlayerChoice == "paper" && computerChoice == "rock"){
+    playerScore++;
     return "Player won! paper beats rock";
    }
    if(fixPlayerChoice == "paper" && computerChoice == "scissors"){
+    cpuScore++;
     return "CPU won! scissors beats rock";
    }
    //Scissors
    if(fixPlayerChoice == "scissors" && computerChoice == "sicssors"){
+    playerScore++;
+    cpuScore++;
     return "it's a draw";
    }
    if(fixPlayerChoice == "scissors" && computerChoice == "paper"){
+    playerScore++;
     return "Player won! scissors beats paper";
    }
    if(fixPlayerChoice == "scissors" && computerChoice == "rock"){
+    cpuScore++;
     return "CPU won! rock beats scissors";
    }
    //Rock
    if(fixPlayerChoice == "rock" && computerChoice == "rock"){
+    playerScore++;
+    cpuScore++;
     return "it's a draw";
    }
    if(fixPlayerChoice == "rock" && computerChoice == "scissors"){
+    playerScore++;
     return "Player won! rock beats scissors";
    }
    if(fixPlayerChoice == "rock" && computerChoice == "paper"){
+    cpuScore++;
     return "CPU won! paper beats rock";
    }
    else{
@@ -43,8 +57,31 @@ function playRound(fixPlayerChoice, computerChoice){
    }
 
 }
-computerChoice = computerPlay();
-console.log("CPU choose: " +  computerChoice);
-console.log("Player choose: "+ fixPlayerChoice);
 
-console.log(playRound(fixPlayerChoice, computerChoice));
+function game(){
+    for (i = 0; i < 5; i++){
+        //Ask player their choice
+        let playerChoice = prompt("Write your choice: ");
+        let fixPlayerChoice = playerChoice.toLowerCase();
+        //Assign computer choice to a variable
+        computerChoice = computerPlay();
+        //Show choices
+        console.log("CPU choose: " +  computerChoice);
+        console.log("Player choose: "+ fixPlayerChoice);
+        //Show result of the round
+        console.log(playRound(fixPlayerChoice, computerChoice));
+    }
+    
+    if (playerScore > cpuScore){
+        console.log("Player won the game!!");
+        console.log("Player: " +playerScore+" CPU: " +cpuScore);
+    }else{
+        console.log("CPU won the game!!");
+        console.log("CPU: " +cpuScore+ " Player: " +playerScore);
+    }
+}
+game();
+
+
+
+
